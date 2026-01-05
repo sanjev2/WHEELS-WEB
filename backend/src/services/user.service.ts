b5bb74a14e9dc42a927a99197b264b17dae7d2e5
Sanjev2 <sanjeevmanandhar53@gmail.com>
2026-01-05 18:38:01 +0545

    service added

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
     contact: data.contact,
     address: data.address,
     password: hashedPassword,
-    role: data.role ?? 'user', 
+    role: data.role ?? 'use', 
   
   }
 
