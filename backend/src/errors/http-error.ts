165271d62898daf3259a72966484095b02ed0374
Sanjev2 <sanjeevmanandhar53@gmail.com>
2026-01-05 18:34:54 +0545

    errors updated

--- backend/src/errors/http-error.ts
+++ backend/src/errors/http-error.ts
@@ -2,6 +2,6 @@ export class HttpError extends Error {
     statusCode: number
     constructor(statusCode: number, message: string) {
         super(message);
-        this.statusCode = statusCod;
+        this.statusCode = statusCode;
     }
 }
