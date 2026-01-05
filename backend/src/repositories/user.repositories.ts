0ae4144ac14dbc1d9900ed6eb4d8687b1b32a65f
Sanjev2 <sanjeevmanandhar53@gmail.com>
2026-01-05 18:36:02 +0545

    repositories

--- backend/src/repositories/user.repositories.ts
+++ backend/src/repositories/user.repositories.ts
@@ -16,7 +16,7 @@ export class UserRepository implements IUserRepository {
   }
 
   async getUserByEmail(email: string): Promise<IUser | null> {
-    const user = await UserModel.findOne({ email: email })
+    const user = await UserModel.findOn({ email: email })
     return user
   }
 
